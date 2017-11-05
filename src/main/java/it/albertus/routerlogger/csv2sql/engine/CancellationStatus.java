5270c8eb82f07cdeb2ac0f8108eed92a12991815
Alberto <albertus82@gmail.com>
2017-11-05 19:39:27 +0100

    Javadoc.

--- src/main/java/it/albertus/routerlogger/csv2sql/engine/CancellationStatus.java
+++ src/main/java/it/albertus/routerlogger/csv2sql/engine/CancellationStatus.java
@@ -1,7 +1,16 @@
 package it.albertus.routerlogger.csv2sql.engine;
 
+@FunctionalInterface
 public interface CancellationStatus {
 
+	/**
+	 * Returns whether cancellation of current operation has been requested.
+	 * Long-running operations should poll to see if cancellation has been
+	 * requested.
+	 *
+	 * @return <code>true</code> if cancellation has been requested, and
+	 *         <code>false</code> otherwise
+	 */
 	boolean isCanceled();
 
 }
