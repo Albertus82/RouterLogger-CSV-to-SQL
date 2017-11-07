da41d5d7d5dd3a561b489f8db60f281528100c88
Alberto <albertus82@gmail.com>
2017-11-07 21:07:38 +0100

    Images

--- src/main/java/it/albertus/routerlogger/csv2sql/gui/Images.java
+++ src/main/java/it/albertus/routerlogger/csv2sql/gui/Images.java
@@ -4,15 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.swt.widgets.Display;
 
+import it.albertus.util.logging.LoggerFactory;
+
 public class Images {
 
-	// Icona principale dell'applicazione (in vari formati)
+	private static final Logger logger = LoggerFactory.getLogger(Images.class);
+
+	// Main application icon (in various formats)
 	private static final Collection<Image> mainIcons = new LinkedHashSet<>();
 
 	private Images() {
@@ -27,7 +33,7 @@ public class Images {
 			}
 		}
 		catch (final IOException e) {
-			throw new IllegalStateException(e);
+			logger.log(Level.WARNING, e.toString(), e);
 		}
 	}
 
