bb02446a427aaa8a29ba15283f47b9f95788766d
Alberto <albertus82@gmail.com>
2017-11-22 18:23:04 +0100

    Refactoring.

--- src/main/java/it/albertus/routerlogger/csv2sql/gui/IGuiDirector.java
+++ src/main/java/it/albertus/routerlogger/csv2sql/gui/IGuiDirector.java
@@ -7,29 +7,29 @@ import org.eclipse.swt.events.SelectionEvent;
 
 interface IGuiDirector {
 
-	void sourceFilesListKeyPressed(KeyEvent e);
-
-	void sourceFilesListSelected(SelectionEvent e);
-
 	void addSourceFilesButtonSelected(SelectionEvent e);
 
-	void removeSourceFilesButtonSelected(SelectionEvent e);
+	void browseDirectoryButtonSelected(SelectionEvent e);
 
 	void clearSourceFilesButtonSelected(SelectionEvent e);
 
-	void sourceFilesListContextMenuDetected(MenuDetectEvent e);
+	void clearSourceFilesMenuItemSelected(SelectionEvent e);
+
+	void closeButtonSelected(SelectionEvent e);
+
+	void processButtonSelected(SelectionEvent e);
+
+	void removeSourceFilesButtonSelected(SelectionEvent e);
 
 	void removeSourceFilesMenuItemSelected(SelectionEvent e);
 
 	void selectAllSourceFilesMenuItemSelected(SelectionEvent e);
 
-	void clearSourceFilesMenuItemSelected(SelectionEvent e);
+	void sourceFilesListContextMenuDetected(MenuDetectEvent e);
 
-	void browseDirectoryButtonSelected(SelectionEvent e);
+	void sourceFilesListKeyPressed(KeyEvent e);
 
-	void processButtonSelected(SelectionEvent e);
-
-	void closeButtonSelected(SelectionEvent e);
+	void sourceFilesListSelected(SelectionEvent e);
 
 	void textModified(ModifyEvent e);
 
